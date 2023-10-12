886fb43812a10a207f925037d9e73ad4ae36ecd9
laubenitez79 <laubenitez@uade.edu.ar>
2023-10-12 00:48:38 -0300

    new proyect shopping cart

---
+++ projects/shopping-cart/src/components/Filters.jsx
@@ -0,0 +1,51 @@
+import { useState, useId } from 'react'
+import './Filters.css'
+
+export function Filters ({ onChange }) {
+    const [minPrice , setMinPrice ] = useState()
+    const categoryFilterId = useId()
+    const minPriceFilterId = useId()
+
+
+    const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+        onChange(prevState => ({
+            ...prevState,
+            minPrice : event.target.value
+        }))
+    }
+
+    const handleChangeCategory = (event) => {
+        onChange(prevState => ({
+            ...prevState,
+            category : event.target.value
+        }))
+    }
+
+
+    return (
+        <section className="filters">
+
+            <div className="">
+                <label htmlFor={minPriceFilterId}>Precio</label>
+                <input 
+                type="range" 
+                id={minPriceFilterId}
+                min='0'
+                max='1000'
+                onChange={handleChangeMinPrice}
+                />
+                <span>{minPrice}$</span>
+            </div>
+
+            <div className="">
+                <label htmlFor={categoryFilterId}>Categoría</label>
+                <select id={categoryFilterId} onChange={handleChangeCategory}>
+                    <option value="all">Todas</option>
+                    <option value="laptops">PCs</option>
+                    <option value="smartphones">Celulares</option>
+                </select>
+            </div>
+        </section>
+    )
+}
